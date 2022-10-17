7bba565fc1667a4e1077b36321a1642f0ba8ed51
talesa365 <talesahowell7@gmail.com>
2022-10-17 16:58:05 -0400

    done

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
 # tip_calculator
 Python tip calculator
-# The challenge
-
-The challenge is to build out a tip calculator app.
 
 
+This is a tip calculator app that takes a user's input at the command line for
 
-Your users should be able to:
-
-- Calculate the correct tip and total cost of the bill per person
-- After a tip has been calculated, your program should ask the user if they would like to enter another tip, and    run again if the answer is yes.
+-Cost of the food
+-Number of people splitting the bill
+-Percentage of the tip
+-After a tip has been calculated, your program should ask the user if they would like to enter another tip, and run again if the answer is yes.
 
 
 
 
+output:
+
+-The total bill (including tip)
+-How much each person should pay (you can assume all people will split the bill evenly)
+ 
+
+Assume there is a 10% sales tax. Don't forget to add this into the total bill! 
+
+Note: your tip calculator should be able to handle a bill of any amount of many money, with any number of people splitting the bill, and with any tip percentage (including 0 tip)
 
 
-## Create a custom `README.md`
+
+
+
+
+
+
+
 
 
 
--- tip_calculator.py
+++ tip_calculator.py
@@ -1,25 +1,17 @@
 
-from pickle import TRUE
 
 
 def calculate():
 
     print("Welcome To The Python Tip Calculator")
         #Prompt the user to enter the amount on the bill
-    meal = float(input("What is the total bill?: "))
+    meal = float(int(input("What is the total bill?: ")))
         #Prompt the user for the % tip they'd like to leave
     tip = int(input("What % tip would you like to give?: "))
         #Prompt the user for the # of people splitting the bill
     people = int(input("How many people are splitting the bill?: "))
           
     
-          
-        
-       
-       
-     
-    
-
     tax = .10
     print(tax)
     #figure the tip on the bill amount pre tax 
@@ -32,36 +24,23 @@ def calculate():
     #update the bill to include the tax   
     print(f' Your total with tax is ${total_cost}')
     bill_per_person = total_cost / people 
-    final_total = round(bill_per_person)
+    final_total = bill_per_person
  
      
     #Calculate the amount each person owes based on the bill and tips the user entered
             
-    print(f'Your total bill is {final_total} and each person should pay {final_total}')
- 
-    
-    
-            
-      
-
-
- 
-    
-   
-   
-        
+    print(f'Your total bill is {total_cost} and each person should pay {final_total}')
  
     # we need to include an option to continue to calculate for other tip amounts
-
-    another_amount = input('Would you like to enter another amount?')
-   
-    if another_amount == "n":
-        print('I hope you have enjoyed using your Python Tip Calculator!')
-        return
-    else:
-        another_amount == "y"
-        return calculate()
-
-        
-            
+ 
+    another_amount = input('Would you like to enter another amount? ')
+    print(another_amount)
+    for letter in another_amount:
+        if letter == "n":
+            print('I hope you have enjoyed using your Python Tip Calculator!')
+            return
+        else:
+            another_amount == "y"
+            calculate()
+      
 calculate()    
