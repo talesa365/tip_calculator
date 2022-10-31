90e94a74e5281b16323fad203e690adf13ff7177
talesa365 <talesahowell7@gmail.com>
2022-10-31 18:03:16 -0400

    re-sub

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,17 +1,15 @@
 
-
-
-def calculate():
-
-    print("Welcome To The Python Tip Calculator")
-        #Prompt the user to enter the amount on the bill
+print("Welcome To The Python Tip Calculator")
+#Create a function to calculate the split costs of the customers bill
+def calculate():    
+    #Prompt the user to enter the amount on the bill
     meal = float(int(input("What is the total bill?: ")))
-        #Prompt the user for the % tip they'd like to leave
+    #Prompt the user for the % tip they'd like to leave
     tip = int(input("What % tip would you like to give?: "))
-        #Prompt the user for the # of people splitting the bill
+    #Prompt the user for the # of people splitting the bill
     people = int(input("How many people are splitting the bill?: "))
-          
-    
+
+   #tax amount was given in the instructions
     tax = .10
     print(tax)
     #figure the tip on the bill amount pre tax 
@@ -23,16 +21,14 @@ def calculate():
     total_cost = tip_amount + tax_amount + meal
     #update the bill to include the tax   
     print(f' Your total with tax is ${total_cost}')
+     #Calculate the amount each person owes based on the bill and tips the user entered     
     bill_per_person = total_cost / people 
     final_total = bill_per_person
- 
-     
-    #Calculate the amount each person owes based on the bill and tips the user entered
-            
     print(f'Your total bill is {total_cost} and each person should pay {final_total}')
+calculate()
  
     # we need to include an option to continue to calculate for other tip amounts
- 
+def ask_again():
     another_amount = input('Would you like to enter another amount? ')
     print(another_amount)
     for letter in another_amount:
@@ -41,6 +37,8 @@ def calculate():
             return
         else:
             another_amount == "y"
-            calculate()
+            result = calculate()
+            return result
+
       
-calculate()    
+ask_again()   
